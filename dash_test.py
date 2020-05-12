550e327b9d0fa2e652d04f401f40b3dd7b5612af
Annika Noren <amenoren@gmail.com>
2020-05-12 16:12:27 -0400

    my changes

--- dash_test.py
+++ dash_test.py
@@ -5,10 +5,10 @@ import pandas as pd
 import plotly.graph_objs as go 
 import base64
 
+# Read in dataframe 
 df = pd.read_csv('cleaned_coffee_reviews.csv')
 df = df.drop(columns=['Unnamed: 0.1','Unnamed: 0.1.1','Unnamed: 0'], axis=1)
 
-# Helpful tutorial: https://www.youtube.com/watch?v=hRH01ZzT2NI
 
 # scatter plot #1
 trace = go.Scatter(x=df["Rating"], y=df["Prices Per Oz"], mode='markers')
@@ -21,17 +21,49 @@ fig.update_layout(height=1000,title_text='Rating vs Prices Per Oz')
 #  https://pbpython.com/plotly-dash-intro.html
 trace2 = go.Bar(x=df['Rating'], y=df['Prices Per Oz'])
 
-# Bean image, couldn't get it bigger
-# image_filename = 'beans_leaf.jpeg' # replace with your own image
-# encoded_image = base64.b64encode(open(image_filename, 'rb').read())
-# html.Img(src='data:image/png;base64,{}, style={'width':'20%'}'.format(encoded_image)),
-
 # Drop down menu options
 features = df[['Flavor','Body','Aroma','Aftertaste','Acidity','Prices Per Oz','Agtron Ground']].copy()
 opts = [{'label' : i, 'value' : i} for i in features]
 
 # Main App
 app = dash.Dash()
+
+app.layout = html.Div(children=[
+    html.Div([
+        html.H1('Finding The Perfect Cup of Coffee'),
+        html.P('Annika M Noren, May 2020')],
+        style = {'padding' : '50px' , 
+                 'backgroundColor' : '#907A1C'}),
+    coffee_graph,
+    dcc.Graph(
+        id='second plot',
+        figure={
+            'data': [trace2],
+            'layout': go.Layout(
+                title='Second Plot', 
+                xaxis = {'title':'Rating'},
+                yaxis = {'title':'Price'},
+                barmode='stack')})
+])
+
+
+
+if __name__ == '__main__':
+    app.run_server(debug = True)
+
+
+
+
+# Extra Code that is obsolete, didn't work or not needed
+
+# Helpful tutorial: https://www.youtube.com/watch?v=hRH01ZzT2NI
+
+# Bean image, couldn't get it bigger
+# image_filename = 'beans_leaf.jpeg' # replace with your own image
+# encoded_image = base64.b64encode(open(image_filename, 'rb').read())
+# html.Img(src='data:image/png;base64,{}, style={'width':'20%'}'.format(encoded_image))
+
+
 # app.layout = html.Div('Coffee')
 
 # app.layout = html.Div(children=[
@@ -46,21 +78,6 @@ app = dash.Dash()
 #     )
 # ])
 
-app.layout = html.Div(children=[
-    html.Div([
-        html.H1('Finding The Perfect Cup of Coffee'),
-        html.P('Annika Noren, May 2020')],
-        style = {'padding' : '50px' , 
-                 'backgroundColor' : '#907A1C'}),
-    dcc.Graph(
-        id='second plot',
-        figure={
-            'data': [trace2],
-            'layout': go.Layout(title='Second Plot', barmode='stack')
-        }
-    )
-])
-
 # app.layout = html.Div([
 #                 # adding a header and a paragraph
 #                 html.Div([
@@ -68,6 +85,3 @@ app.layout = html.Div(children=[
 #                     style = {'padding' : '50px' , 
 #                              'backgroundColor' : '#907A1C'})
 # ])
-
-if __name__ == '__main__':
-    app.run_server(debug = True)
